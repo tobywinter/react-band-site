775e2f400a9f2aecdbbb68afd8d1e0f6b076f01f
Toby Winter <toby.hw@live.co.uk>
2018-07-31 03:20:38 +0100

    YWN005 :sparkles: adds multiple art/links to listen page using cssgrid

--- src/components/Listen.js
+++ src/components/Listen.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 
 import CoverArt from "./CoverArt";
 import Logo from "./Logo";
-import Quote from "./Quote";
-import "./Home.css";
+import "./Listen.css";
 const partisanArtwork = require("./assets/partisan-artwork.png");
+const beAManArtwork = require("./assets/be-a-man-art.png");
 
 export default class Home extends Component {
   render() {
@@ -14,11 +14,18 @@ export default class Home extends Component {
           <h2 className="title">
             <Logo colour="black" />
           </h2>
-          <CoverArt
-            className="art"
-            src={partisanArtwork}
-            link={`http://hyperurl.co/obtic5`}
-          />
+          <div className="wrapper">
+            <CoverArt
+              className="art"
+              src={partisanArtwork}
+              link={`http://hyperurl.co/obtic5`}
+            />
+            <CoverArt
+              className="art"
+              src={beAManArtwork}
+              link={`http://hyperurl.co/atbw3u`}
+            />
+          </div>
         </div>
       </div>
     );
